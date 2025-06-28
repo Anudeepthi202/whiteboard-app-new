1821a73153346aa87ae21c7a388eb44540dd45a3
Anu Deepthi <anudeepthi202@gmail.com>
2025-06-28 14:36:01 +0530

    fix: added / route and dynamic port for Render

--- server/server.js
+++ server/server.js
@@ -18,7 +18,8 @@ const io = new Server(server, {
 app.use(cors());
 app.use(express.json());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 
 // 🧠 In-memory room store
 let rooms = {};
